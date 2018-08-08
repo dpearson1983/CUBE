27620544cfb75e2e4f8b36a1a3be08a8353973a8
David Pearson <dpearson@localhost.localdomain>
2018-08-08 14:34:40 -0500

    Finished implementation of Bianchi/Scoccimarro line-of-sight method

--- include/line_of_sight.h
+++ include/line_of_sight.h
@@ -1,10 +1,13 @@
 #ifndef _LINE_OF_SIGHT_H_
 #define _LINE_OF_SIGHT_H_
 
-void get_A0(std::vector<double> &dr, std::vector<double> &A_0, int3 N, double3 L);
+#include <vector>
+#include <vector_types.h>
 
-void get_A2(std::vector<double> &dr, std::vector<double> &A_2, int3 N, double3 L);
+void get_A0(std::vector<double> &dr, std::vector<double> &A_0, int3 N);
 
-void get_A4(std::vector<double> &dr, std::vector<double> &A_4, int3 N, double3 L);
+void get_A2(std::vector<double> &dr, std::vector<double> &A_2, int3 N, double3 L, double3 r_min);
+
+void get_A4(std::vector<double> &dr, std::vector<double> &A_4, int3 N, double3 L, double3 r_min);
 
 #endif
