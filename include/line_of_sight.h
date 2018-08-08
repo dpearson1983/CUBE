948225f460719d69e6e21e58b6a554d9cda6d1bb
David Pearson <dpearson@localhost.localdomain>
2018-08-08 11:43:23 -0500

    Add start of implementation of Bianchi/Scoccimarro method of encoding line-of-sight information

---
+++ include/line_of_sight.h
@@ -0,0 +1,10 @@
+#ifndef _LINE_OF_SIGHT_H_
+#define _LINE_OF_SIGHT_H_
+
+void get_A0(std::vector<double> &dr, std::vector<double> &A_0, int3 N, double3 L);
+
+void get_A2(std::vector<double> &dr, std::vector<double> &A_2, int3 N, double3 L);
+
+void get_A4(std::vector<double> &dr, std::vector<double> &A_4, int3 N, double3 L);
+
+#endif
